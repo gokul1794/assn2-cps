6cec149df9a45c95c925f3061b0f464ee0e06566
Gokul Shanth <gokul.shanth@gmail.com>
2020-02-14 15:31:32 -0500

    solved question 8

--- assn2-cps/solns/DictSearch.cs
+++ assn2-cps/solns/DictSearch.cs
@@ -8,7 +8,44 @@ namespace assn2_cps.solns
     {
         public static bool DictSearchSoln(string[] userDict, string keyword)
         {
-            throw new NotImplementedException();
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i = 0; i < userDict.Length; i++)
+            {
+                dict.Add(userDict[i], i);
+            }
+            if (dict.ContainsKey(keyword))
+                return false;
+            else
+            {
+                int count = 0;
+                string correctWord = userDict[0];
+                for (int i = 0; i < userDict.Length; i++)
+                {
+                    if (keyword.Length == userDict[i].Length)
+                    {
+                        string dictWord = userDict[i];
+                        for (int j = 0; j < dictWord.Length; j++)
+                        {
+                            if (keyword[j] == dictWord[j])
+                            {
+                                count+=1;
+                                correctWord = dictWord;
+                            }
+                        }
+                        if (count == keyword.Length - 1)
+                        {
+                            Console.WriteLine(correctWord);
+                            return true;
+                        }
+                        else
+                        {
+                            count = 0;
+                        }
+                    }
+                }
+            }
+            return false;
+            //throw new NotImplementedException();
         }
     }
 }
