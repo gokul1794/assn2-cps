6ada90226522cb5ddda9a2e3042f9edea38b294a
jesidacosta <jvac06@gmail.com>
2020-02-16 19:46:10 -0500

    solved problem 7

--- assn2-cps/solns/TargetRange.cs
+++ assn2-cps/solns/TargetRange.cs
@@ -8,18 +8,19 @@ namespace assn2_cps.solns
     {
         public static int[] TargetRangeSoln(int[] l1, int target)
         {
-            int first=-1, final=-1;
+            int first=-1, final=-1; //if no target variable are found output: [-1,-1]
             int counter = 0;
             for (int i=0; i < l1.Length; i++)
             {
                 if (counter == 0 && l1[i] == target)
                 {
-                    first = i;
+                    //if only one number matches target on the list, it should populate as [first,final]
+                    first = i; 
                     final = i;
                     counter++ ;
                     continue;
                 }
-                if (counter > 0 && l1[i] == target)
+                if (counter > 0 && l1[i] == target) //change final value if more target variables are found in ascending array
                 {
                     final = i;
                 }
