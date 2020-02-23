f142970c14d2e650a883fed049b238ef1db9cff5
jesidacosta <jvac06@gmail.com>
2020-02-23 15:53:55 -0500

    Checked corner cases prob: 1,2,3,4,7

--- assn2-cps/solns/ContainsDuplicate.cs
+++ assn2-cps/solns/ContainsDuplicate.cs
@@ -8,7 +8,49 @@ namespace assn2_cps.solns
     {
         public static bool ContainsDuplicateSoln(char[] arr, int k)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Dictionary<char, int> myDict = new Dictionary<char, int>();
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (!myDict.ContainsKey(arr[i]))
+                    {
+                        myDict.Add(arr[i], i);
+                    }
+
+                    else
+                    {
+                        int v = myDict[arr[i]];
+                        int new_v = i - v;
+                        myDict[arr[i]] = new_v;
+                        Console.WriteLine(myDict[arr[i]]);
+                        if (myDict[arr[i]] <= k)
+                        {
+                            return true;
+                        }
+
+
+                    }
+
+
+                }
+                return false;
+                //foreach (KeyValuePair<char, int> kvp in myDict)
+                // {
+                //     Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                //}
+
+
+
+
+
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
--- assn2-cps/solns/MinimumSum.cs
+++ assn2-cps/solns/MinimumSum.cs
@@ -8,7 +8,37 @@ namespace assn2_cps.solns
     {
         public static int MinimumSumSoln(int[] l2)
         {
-            throw new NotImplementedException();
+            int sum;
+            try
+            {
+                sum = l2[0];
+                int i;
+
+                for (i = 0; i < l2.Length - 1; i++)
+                {
+                    if (l2[i + 1] == l2[i])
+                    {
+                        l2[i + 1] = l2[i] + 1;
+                        if (i < l2.Length - 2)
+                        {
+                            if (l2[i + 2] < l2[i + 1])
+                            {
+                                int c = l2[i + 1] - l2[i + 2];
+                                l2[i + 2] = c + l2[i + 2];
+                            }
+                        }
+                    }
+                    sum = sum + l2[i + 1];
+                }
+            }
+            catch (Exception e)
+            {
+                //Message in case list is empty. Sum of the array is 0
+                Console.WriteLine($"Error: {e.GetType().Name}, check list has at least one value");
+                sum = 0;
+            }
+            return sum;
         }
+
     }
 }
